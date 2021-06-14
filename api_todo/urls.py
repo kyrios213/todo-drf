b5182270a309cd3c560546975a2da74f6b0d725e
Lance Pidor <kyrios213@gmail.com>
2021-06-14 17:00:39 +0800

    Frontend added

--- api_todo/urls.py
+++ api_todo/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('task/', views.task_list, name='task_list'),
     path('task/<str:pk>/', views.task_detail, name='task_detail'),
     path('task-create/', views.task_create, name='task_create'),
+    
     path('task-update/<str:pk>/', views.task_update, name='task_update'),
     path('task-delete/<str:pk>/', views.task_delete, name='task_delete'),
 
